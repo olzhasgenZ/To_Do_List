c81cac3ac5dde235b4cf7031c191205c27d99a39
unknown <olzhassarsenbaev08@gmail.com>
2025-05-11 21:50:35 +0500

    first

---
+++ app/keyboards.py
@@ -0,0 +1,11 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from app.database.requests import get_tasks
+
+async def tasks(tg_id):
+    tasks = await get_tasks(tg_id)
+    keyboard = InlineKeyboardBuilder()
+    for task in tasks:
+        keyboard.add(InlineKeyboardButton(text=task.task, callback_data=f'task_{task.id}'))
+    return keyboard.adjust(1).as_markup()
---
+++ app/user.py
@@ -0,0 +1,31 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart, Command
+
+from app.database.requests import set_user, del_task, set_task
+import app.keyboards as kb
+
+user = Router()
+
+
+@user.message(CommandStart())
+async def cmd_start(message: Message):
+    await set_user(message.from_user.id)
+    await message.answer('Нажмите на выполненную задачу чтобы удалить или напишите в чат новую',
+                         reply_markup=await kb.tasks(message.from_user.id))
+
+@user.callback_query(F.data.startswith('task_'))
+async def delete_task(callback: CallbackQuery):
+    await del_task(callback.data.split('_')[1])
+    await callback.message.delete()
+    await callback.message.answer('Нажмите на выполненную задачу чтобы удалить или напишите в чат новую',
+                         reply_markup=await kb.tasks(callback.from_user.id))
+    await callback.answer('Задача выполнена')
+
+@user.message()
+async def add_task(message: Message):
+    if len(message.text) > 100:
+        await message.answer('Задача слишком длинная!')
+        return
+    await set_task(message.from_user.id, message.text)
+    await message.answer('Задача добавлена\nНажмите на выполненную задачу чтобы удалить или напишите в чат новую', reply_markup=await kb.tasks(message.from_user.id))
